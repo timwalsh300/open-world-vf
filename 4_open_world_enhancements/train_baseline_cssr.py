0a46355f10a4ee28790782a2c0d06c64362c9a83
timwalsh300 <timwalsh300@gmail.com>
2024-11-18 17:24:16 -0800

    CSSR with jointly trained feature extraction layers

--- 4_open_world_enhancements/train_baseline_cssr.py
+++ 4_open_world_enhancements/train_baseline_cssr.py
@@ -12,13 +12,13 @@ INPUT_SHAPES = {'schuster8': (1, 1920),
                 
 protocol = sys.argv[1]
 
-# we manually copy and paste these hyperparameters from the output of search_open.py
-BEST_HYPERPARAMETERS = {'schuster8_tor': {'filters': 256, 'kernel': 8, 'conv_stride': 1, 'pool': 8, 'pool_stride': 4, 'conv_dropout': 0.1, 'fc_neurons': 128, 'fc_init': 'he_normal', 'fc_activation': 'elu', 'fc_dropout': 0.1, 'lr': 7.191906601911815e-05, 'batch_size': 128},
-                        'dschuster16_https': {'filters': 256, 'kernel': 4, 'conv_stride': 2, 'pool': 8, 'pool_stride': 1, 'conv_dropout': 0.4, 'fc_neurons': 1024, 'fc_init': 'glorot_uniform', 'fc_activation': 'relu', 'fc_dropout': 0.8, 'lr': 0.0005153393428807454, 'batch_size': 64}}
+# we manually copy and paste these hyperparameters from the output of search.py
+BASELINE_MONITORED_HYPERPARAMETERS = {'schuster8_tor': {'filters': 256, 'kernel': 8, 'conv_stride': 2, 'pool': 8, 'pool_stride': 1, 'conv_dropout': 0.1, 'fc_neurons': 128, 'fc_init': 'he_normal', 'fc_activation': 'relu', 'fc_dropout': 0.4, 'lr': 0.00014940538775594852, 'batch_size': 128},
+                            'dschuster16_https': {'filters': 256, 'kernel': 4, 'conv_stride': 1, 'pool': 16, 'pool_stride': 4, 'conv_dropout': 0.2, 'fc_neurons': 256, 'fc_init': 'he_normal', 'fc_activation': 'elu', 'fc_dropout': 0.6, 'lr': 6.0487458111998665e-05, 'batch_size': 32}}
 
 # helpfully provided by ChatGPT
 class EarlyStopping:
-    def __init__(self, patience=10, verbose=True, delta=0, path='checkpoint.pt', trace_func=print):
+    def __init__(self, patience=10, verbose=True, delta=0, path_backbone='backbone_checkpoint.pt', path_classifier='classifier_checkpoint.pt', trace_func=print):
         self.patience = patience
         self.verbose = verbose
         self.counter = 0
@@ -26,13 +26,14 @@ class EarlyStopping:
         self.early_stop = False
         self.val_loss_min = numpy.Inf
         self.delta = delta
-        self.path = path
+        self.path_backbone = path_backbone
+        self.path_classifier = path_classifier
         self.trace_func = trace_func
-    def __call__(self, val_loss, model):
+    def __call__(self, val_loss, backbone, classifier):
         score = -val_loss
         if self.best_score is None:
             self.best_score = score
-            self.save_checkpoint(val_loss, model)
+            self.save_checkpoint(val_loss, backbone, classifier)
         elif score <= self.best_score + self.delta:
             self.counter += 1
             self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
@@ -40,13 +41,14 @@ class EarlyStopping:
                 self.early_stop = True
         else:
             self.best_score = score
-            self.save_checkpoint(val_loss, model)
+            self.save_checkpoint(val_loss, backbone, classifier)
             self.counter = 0
-    def save_checkpoint(self, val_loss, model):
+    def save_checkpoint(self, val_loss, backbone, classifier):
         if self.verbose:
-            #self.trace_func(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
-            self.trace_func(f'Validation acc increased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
-        torch.save(model.state_dict(), self.path)
+            self.trace_func(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving models...')
+            #self.trace_func(f'Validation acc increased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving models...')
+        torch.save(backbone.state_dict(), self.path_backbone)
+        torch.save(classifier.state_dict(), self.path_classifier)
         self.val_loss_min = val_loss
 
 print(torch.cuda.is_available())
@@ -62,7 +64,7 @@ for representation in ['dschuster16', 'schuster8']:
             val_tensors = torch.load(representation + '_' + protocol + '_val_mon_tensors.pt')
             val_dataset = torch.utils.data.TensorDataset(*val_tensors)
             train_loader = torch.utils.data.DataLoader(train_dataset,
-                                                       batch_size = BEST_HYPERPARAMETERS[representation + '_' + protocol]['batch_size'],
+                                                       batch_size = BASELINE_MONITORED_HYPERPARAMETERS[representation + '_' + protocol]['batch_size'],
                                                        shuffle=False)
             val_loader = torch.utils.data.DataLoader(val_dataset,
                                                      batch_size = len(val_dataset),
@@ -77,23 +79,23 @@ for representation in ['dschuster16', 'schuster8']:
         #    print('...gamma', gamma)
         for trial in range(10):
             print('...trial', trial)
-            # load the pre-trained model that does feature extraction
+            # instantiate the feature extraction layers
             backbone = mymodels_torch.DFNetTunable(INPUT_SHAPES[representation],
                                                    60,
-                                                   BEST_HYPERPARAMETERS[representation + '_' + protocol])
-            backbone.load_state_dict(torch.load(representation + '_' + protocol + '_baseline_monitored_model' + str(trial) + '.pt'))
+                                                   BASELINE_MONITORED_HYPERPARAMETERS[representation + '_' + protocol])
             backbone.to(device)
-            backbone.eval()
             # instantiate the CSSR classifier
             classifier = mymodels_torch.CSSRClassifier(in_channels=256, num_classes=60, hidden_layers=[], latent_channels=32, gamma=0.1)
             classifier.to(device)
             criterion = torch.nn.CrossEntropyLoss()
-            optimizer = torch.optim.Adam(classifier.parameters(), lr=0.001)
+            optimizer = torch.optim.Adam(list(backbone.parameters()) + list(classifier.parameters()), lr=BASELINE_MONITORED_HYPERPARAMETERS[representation + '_' + protocol]['lr'])
             early_stopping = EarlyStopping(patience = 60,
                                            verbose = True,
-                                           path = (representation + '_' + protocol + '_baseline_cssr_model' + str(trial) + '.pt'))
+                                           path_backbone = (representation + '_' + protocol + '_baseline_cssr_backbone_model' + str(trial) + '.pt'),
+                                           path_classifier = (representation + '_' + protocol + '_baseline_cssr_classifier_model' + str(trial) + '.pt'))
             print('Starting to train now for', representation, protocol)
             for epoch in range(240):
+                backbone.train()
                 classifier.train()
                 training_loss = 0.0
                 for x_train, y_train in train_loader:
@@ -108,6 +110,7 @@ for representation in ['dschuster16', 'schuster8']:
                 val_loss = 0.0
                 correct_predictions = 0
                 total_predictions = 0
+                backbone.eval()
                 classifier.eval()
                 with torch.no_grad():
                     for x_val, y_val in val_loader:
@@ -125,11 +128,11 @@ for representation in ['dschuster16', 'schuster8']:
                 # Calculate and print accuracy
                 accuracy = correct_predictions / total_predictions
                 print(f'Epoch {epoch+1} \t Training Loss: {training_loss / len(train_dataset)} \t Validation Loss: {val_loss / len(val_dataset)} \t Accuracy: {accuracy:.4f}')
-                # check if this is a new low validation loss and, if so, save the model
+                # check if this is a new low validation loss and, if so, save the models
                 #
                 # otherwise increment the counter towards the patience limit
-                #early_stopping(val_loss, classifier)
-                early_stopping(-accuracy, classifier)
+                early_stopping(val_loss / len(val_dataset), backbone, classifier)
+                #early_stopping(-accuracy, classifier)
                 if early_stopping.early_stop:
                     # we've reached the patience limit
                     print('Early stopping')
